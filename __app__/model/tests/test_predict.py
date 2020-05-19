28b44309574039f68a5f25ef15cf23fb4bc82600
Random Azure Script <61463978+rndazurescript@users.noreply.github.com>
2020-05-19 14:25:32 +0300

    added basic test

---
+++ __app__/model/tests/test_predict.py
@@ -0,0 +1,20 @@
+import sys,json;
+from model.predict import predict_image_from_url;  
+
+cat_image='https://raw.githubusercontent.com/Azure-Samples/functions-python-tensorflow-tutorial/master/resources/assets/samples/cat1.png'
+dog_image='https://raw.githubusercontent.com/Azure-Samples/functions-python-tensorflow-tutorial/master/resources/assets/samples/dog1.png'
+
+def test_model_can_predict_cats:
+    results = predict_image_from_url(cat_image); 
+    print(json.dumps(results));
+    assert('cat' == results['predictedTagName'])
+    
+def test_model_can_predict_dogs:
+    results = predict_image_from_url(dog_image); 
+    print(json.dumps(results));
+    assert('dog' == results['predictedTagName'])
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main()
+ 
