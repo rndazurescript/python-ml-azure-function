c655fa15ef3244ff9dbbb9340d6a4c3e8ebb271a
Random Azure Script <61463978+rndazurescript@users.noreply.github.com>
2020-05-19 14:27:46 +0300

    Make tests a function
    Fix python syntax error

--- __app__/model/tests/test_predict.py
+++ __app__/model/tests/test_predict.py
@@ -4,12 +4,12 @@ from model.predict import predict_image_from_url;
 cat_image='https://raw.githubusercontent.com/Azure-Samples/functions-python-tensorflow-tutorial/master/resources/assets/samples/cat1.png'
 dog_image='https://raw.githubusercontent.com/Azure-Samples/functions-python-tensorflow-tutorial/master/resources/assets/samples/dog1.png'
 
-def test_model_can_predict_cats:
+def test_model_can_predict_cats():
     results = predict_image_from_url(cat_image); 
     print(json.dumps(results));
     assert('cat' == results['predictedTagName'])
     
-def test_model_can_predict_dogs:
+def test_model_can_predict_dogs():
     results = predict_image_from_url(dog_image); 
     print(json.dumps(results));
     assert('dog' == results['predictedTagName'])
